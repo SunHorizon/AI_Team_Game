4966610606f3d006ee1cfa213cd54ff39f0d950f
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2019-11-16 11:05:29 -0500

    added first enemy

---
+++ AI_TeamGame/Assets/Scripts/AI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+
+    private Vector3 orientation = Vector3.up;
+    [SerializeField] private float speed;
+    private Vector3 velocity;
+    [SerializeField] private string name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        orientation = Vector3.down * speed;
+        UpdateOrientation();
+
+        velocity = orientation.normalized * speed;
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    void UpdateOrientation()
+    {
+        Vector3 angle = new Vector3(0, 0, -Mathf.Atan2(orientation.x, orientation.y) * Mathf.Rad2Deg);
+        transform.eulerAngles = angle;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == name)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
