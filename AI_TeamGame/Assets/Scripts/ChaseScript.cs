83c3b08167bc18b0b1e7a6eed913394ebbd714b5
Pitifulcube92 <nicodelgado1283>
2019-11-25 04:05:30 -0500

    Just Added Basic chas and Evade Script

---
+++ AI_TeamGame/Assets/Scripts/ChaseScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseScript : MonoBehaviour
+{
+    [Header("Info")]
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 orientation;
+    [SerializeField] private float angularVelocitiy;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orientation = Vector3.up;
+    }
+
+    private void ChaseTarget(Vector3 target_)
+    {
+        float dt = Time.deltaTime;
+
+        Vector3 dir = target_ - gameObject.transform.position;
+
+        float Dot = Vector3.Dot(dir, orientation);
+        float e = Dot / (dir.magnitude * orientation.magnitude);
+        float disiredAngle = Mathf.Acos(e) * Mathf.Rad2Deg;
+        float accurateAngle = angularVelocitiy * dt;
+
+        if (disiredAngle < accurateAngle)
+        {
+            orientation = dir.normalized;
+        }
+        if (disiredAngle > accurateAngle)
+        {
+            Vector3 normalDir = new Vector3(orientation.y, -orientation.x, 0.0f);
+
+            if (Vector3.Dot(normalDir, dir) > 0.0f)
+            {
+                accurateAngle = -accurateAngle;
+            }
+            Vector3 rotatedOrientation = Quaternion.Euler(0, 0, accurateAngle) * orientation.normalized;
+
+            orientation = rotatedOrientation;
+        }
+
+        UpdateOrientation();
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = orientation.normalized * speed;
+
+        pos += velocity * dt;
+        transform.position = pos;
+        //Debug.Log("Target position: " + target_);
+    }
+    void UpdateOrientation()
+    {
+        Vector3 angle = new Vector3(0, 0, -Mathf.Atan2(orientation.x, orientation.y) * Mathf.Rad2Deg);
+        transform.eulerAngles = angle;
+    }
+}
---
+++ AI_TeamGame/Assets/Scripts/EvadeScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvadeScript : MonoBehaviour
+{
+    [Header("Info")]
+    [SerializeField] private GameObject target;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 orientation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orientation = Vector3.up;
+    }
+    private void EvadeTarget(Vector3 target)
+    {
+        float dt = Time.deltaTime;
+
+        // To do: Get the direction(dir) pointing to this object from the target.
+        // The direction is exactly an opposite direction vector of chasing.
+        // The direction should be a unit vector.
+        Vector3 dir = gameObject.transform.position - target;
+
+        orientation = dir;
+        UpdateOrientation();
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = orientation.normalized * speed;
+        // To do: Update the current position (pos) using S_final = S_initial + v * t
+
+
+        pos += velocity * dt;
+        transform.position = pos;
+    }
+
+    void UpdateOrientation()
+    {
+        Vector3 angle = new Vector3(0, 0, -Mathf.Atan2(orientation.x, orientation.y) * Mathf.Rad2Deg);
+        transform.eulerAngles = angle;
+    }
+}
